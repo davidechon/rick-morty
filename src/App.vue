b4decdf2d21e512bd887210aa040e26f67c47935
Davide Chon <57179442+davidechon@users.noreply.github.com>
2022-05-06 10:49:14 +0200

    up

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <HeaderComponent :title="titolo"/>
+  </div>
+</template>
+
+<script>
+import HeaderComponent from './components/HeaderComponent.vue'; 
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComponent
+  },
+  data(){
+    return {
+      titolo: "Rick and Morty",
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "@/assets/style/general.scss";
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <h1 class="text-center my-5">{{title}}</h1>
+  </header>
+
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent',
+  props:[
+    "title",
+
+  ]
+}
+</script>
+
+
+<style lang="scss" scoped>
+header {
+  h1 {
+  font-weight: 700;
+}
+}
+
+</style>
